1e8c36b263114dbbff5d99eabefde51aa657d4df
Benjamin Skinner <benjamin.skinner96@gmail.com>
2020-11-25 23:03:54 -0500

    Temporarily removed texture memory stat due to crash

--- plugin/hdCycles/renderParam.cpp
+++ plugin/hdCycles/renderParam.cpp
@@ -807,13 +807,18 @@ HdCyclesRenderParam::GetRenderStats() const
 {
     ccl::RenderStats stats;
     m_cyclesSession->collect_statistics(&stats);
-    
+
     return {
-        {"hdcycles:version", VtValue(HD_CYCLES_VERSION)},
-        {"hdcycles:geometry:total_memory", VtValue(ccl::string_human_readable_size(stats.mesh.geometry.total_size).c_str())},
-        {"hdcycles:textures:total_memory", VtValue(ccl::string_human_readable_size(stats.image.textures.total_size).c_str())},
-        {"hdcycles:scene:num_objects", VtValue(m_cyclesScene->objects.size())},
-        {"hdcycles:scene:num_shaders", VtValue(m_cyclesScene->shaders.size())},
+        { "hdcycles:version", VtValue(HD_CYCLES_VERSION) },
+        { "hdcycles:geometry:total_memory",
+          VtValue(ccl::string_human_readable_size(stats.mesh.geometry.total_size)
+                      .c_str()) },
+        /*{ "hdcycles:textures:total_memory",
+          VtValue(
+              ccl::string_human_readable_size(stats.image.textures.total_size)
+                  .c_str()) },*/
+        { "hdcycles:scene:num_objects", VtValue(m_cyclesScene->objects.size()) },
+        { "hdcycles:scene:num_shaders", VtValue(m_cyclesScene->shaders.size()) },
     };
 }
 
