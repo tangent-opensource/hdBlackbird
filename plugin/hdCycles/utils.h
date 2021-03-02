c21f6df85f7934845cf494a2f164b7d1fa1ec0a8
Piotr Barejko <piotrbarejko@protonmail.com>
2021-03-02 09:46:11 -0800

    remove populate attrib

--- plugin/hdCycles/utils.h
+++ plugin/hdCycles/utils.h
@@ -407,13 +407,6 @@ HdCyclesIsPrimvarExists(TfToken const& a_name,
 using HdCyclesSampledPrimvarType
     = HdTimeSampleArray<VtValue, HD_CYCLES_MAX_PRIMVAR_SAMPLES>;
 
-/* ======= Attribute Utils ======== */
-
-void
-_PopulateAttribute(const TfToken& name, const TfToken& role,
-                   HdInterpolation interpolation, const VtValue& value,
-                   ccl::Attribute* attr, HdCyclesMesh* mesh);
-
 /* ======== VtValue Utils ========= */
 
 template<typename F>
