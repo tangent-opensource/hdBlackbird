e5a91efd424852e41f628dd9a566b96163017da4
Stefan Werner <stefan.werner@tangent-animation.com>
2021-04-13 07:24:19 -0400

    Fixed more warnings under Windows

--- plugin/hdCycles/material.cpp
+++ plugin/hdCycles/material.cpp
@@ -109,7 +109,7 @@ IsValidCyclesIdentifier(const std::string& identifier)
 
     // DEPRECATED:
     // Only needed for retroactive support of pre 0.8.0 cycles shaders
-    isvalid += (identifier.rfind("cycles:") == 0);
+    isvalid |= (identifier.rfind("cycles:") == 0);
 
     return isvalid;
 }
