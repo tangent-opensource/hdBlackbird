2ccf2b1e9537c42e9483289bbc04b54e0da84025
piotr.barejko <piotr.barejko@tangent-animation.com>
2021-04-15 13:32:14 -0500

    pluginfo

--- plugin/usdCycles/moduleDeps.cpp
+++ plugin/usdCycles/moduleDeps.cpp
@@ -17,7 +17,6 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-
 #include <pxr/pxr.h>
 #include <pxr/base/tf/registryManager.h>
 #include <pxr/base/tf/scriptModuleLoader.h>
