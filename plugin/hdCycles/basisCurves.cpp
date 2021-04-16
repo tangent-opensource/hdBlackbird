09b872c4d0007a35bfc96a49959f2c3badacf3f1
Piotr Barejko <piotrbarejko@protonmail.com>
2021-04-16 13:41:27 -0700

    fix license

--- plugin/hdCycles/basisCurves.cpp
+++ plugin/hdCycles/basisCurves.cpp
@@ -1,4 +1,4 @@
-//  Copyright 2020 Tangent Animation
+//  Copyright 2021 Tangent Animation
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
