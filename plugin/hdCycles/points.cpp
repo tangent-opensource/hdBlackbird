ddfe37c0da449aed0462d0580996684aeb2618f6
Stefan Werner <stefan.werner@tangent-animation.com>
2021-05-21 09:16:26 -0400

    Followup to my last commit - fixed compile error.

--- plugin/hdCycles/points.cpp
+++ plugin/hdCycles/points.cpp
@@ -747,7 +747,7 @@ HdCyclesPoints::Sync(HdSceneDelegate* sceneDelegate, HdRenderParam* renderParam,
             auto value = GetPrimvar(sceneDelegate, description.name);
 
             if (value.IsEmpty()) {
-                TF_WARN("Primvar %s is empty with interpolation %s", description.name.c_str(),
+                TF_WARN("Primvar %s is empty with interpolation %s", description.name.GetText(),
                         _HdInterpolationStr(interpolation));
                 continue;
             }
