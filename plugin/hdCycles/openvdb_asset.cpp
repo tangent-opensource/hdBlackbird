fb77718eaec41835cef29a2bc8a2889829450c95
Stefan Werner <stefan.werner@tangent-animation.com>
2021-06-24 15:07:26 +0200

    Fixed typo

--- plugin/hdCycles/openvdb_asset.cpp
+++ plugin/hdCycles/openvdb_asset.cpp
@@ -17,7 +17,7 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 
-#ifndef APPLE
+#ifndef __APPLE__
 #ifdef Houdini_FOUND
 #define HOUDINI_VDB
 #endif
